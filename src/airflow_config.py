63a5ca1a211877d11f72230c9d81cbf4dd9cda4c
eol <eric.oliveira.lopes@gmail.com>
2019-06-29 17:35:04 +0800

    v1.0

--- src/airflow_config.py
+++ src/airflow_config.py
@@ -2,28 +2,30 @@
 
 import configparser
 import os
+import subprocess
 
 AIRFLOW_HOME = os.environ.get('AIRFLOW_HOME')
-cfg_path = os.path.join(AIRFLOW_HOME, 'airflow.cfg')
+CONFIG_PATH = os.path.join(AIRFLOW_HOME, 'airflow.cfg')
 
 config = configparser.ConfigParser(allow_no_value=True)
-config.read_file(open(cfg_path, 'r'))
+config.read_file(open(CONFIG_PATH, 'r'))
 for k in os.environ.keys():
     if k.startswith('AIRFLOW__'):
         value = os.environ[k]
         _, section, conf = k.lower().split('__')
         config[section][conf] = value
-config.write(open(cfg_path, 'w'))
+config.write(open(CONFIG_PATH, 'w'))
 
 airflow_user = os.environ.get('AIRFLOW_USER')
-if airflow_user:
+airflow_password = os.environ.get('AIRFLOW_PASSWORD')
+if airflow_user and airflow_password:
     import airflow
     from airflow import models, settings
     from airflow.contrib.auth.backends.password_auth import PasswordUser
 
     user = PasswordUser(models.User())
     user.username = airflow_user
-    user.password = os.environ.get('AIRFLOW_PASSWORD')
+    user.password = airflow_password
     user.email = os.environ.get('AIRFLOW_EMAIL')
     user.superuser = True
     session = settings.Session()
---
+++ stacks/airflow-service/create_env.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+environment_docker_image=airflow:1.10.3
+environment_password_size=32
+environment_base_env_path=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
+environment_source_rel_path=../..
+environment_file_name=${1:-env.default}
+
+[ ! -z $(docker images -q ${environment_docker_image}) ] || docker build -t ${environment_docker_image} "${environment_base_env_path}/${environment_source_rel_path}/src"
+
+echo "# creating environment ${environment_base_env_path}/${environment_file_name}"
+
+# host configuration
+export REL_HOST_PATH=${environment_source_rel_path}
+export AIRFLOW_HOST_PORT=8080
+
+# airflow configuration
+export AIRFLOW__CORE__DAGS_FOLDER=/dags
+export AIRFLOW__CORE__EXECUTOR=CeleryExecutor
+export AIRFLOW__CORE__FERNET_KEY=$(docker run --rm --entrypoint='' ${environment_docker_image} python3 -c 'from cryptography.fernet import Fernet; FERNET_KEY=Fernet.generate_key().decode(); print(str(FERNET_KEY))')
+export AIRFLOW__CORE__LOAD_EXAMPLES=True
+export AIRFLOW_PASSWORD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c ${environment_password_size})
+export AIRFLOW_USER=airflow-user
+export AIRFLOW__WEBSERVER__AUTH_BACKEND=airflow.contrib.auth.backends.password_auth
+export AIRFLOW__WEBSERVER__AUTHENTICATE=True
+export AIRFLOW__WEBSERVER__BASE_URL=http://0.0.0.0:8080
+export AIRFLOW__WEBSERVER__WEB_SERVER_PORT=8080
+
+# postgres configuration
+export PGDATA=/var/lib/postgresql/data
+export POSTGRES_DB=airflow
+export POSTGRES_HOST=postgres
+export POSTGRES_PASSWORD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c ${environment_password_size})
+export POSTGRES_PORT=5432
+export POSTGRES_USER=airflow
+
+# redis configuration
+export REDIS_HOST=redis
+export REDIS_PASSWORD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c ${environment_password_size})
+export REDIS_PORT=6379
+
+# traefik configuration
+export TRAEFIK_AUTH_USER=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c ${environment_password_size})
+export TRAEFIK_AUTH_PASSWORD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c ${environment_password_size})
+export TRAEFIK_AUTH_BASIC=$(docker run --rm --entrypoint htpasswd registry:2 -Bbn ${TRAEFIK_AUTH_USER} ${TRAEFIK_AUTH_PASSWORD})
+export TRAEFIK_DOMAIN=airflow.localhost
+export TRAEFIK_ENABLE=true
+
+
+printenv | sort | sed -e "s/^[^#].*/export &/g" | sed -e "s/.*/&'/g" | sed "s/=/='/" > "${environment_base_env_path}/${environment_file_name}"
+
