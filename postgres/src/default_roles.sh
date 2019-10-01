8d738194ef417ad6221c511dc915c7e522b28ed8
nullhack <eric.oliveira.lopes@gmail.com>
2019-10-02 07:17:50 +0800

    improving default roles

--- postgres/src/default_roles.sh
+++ postgres/src/default_roles.sh
@@ -1,55 +1,50 @@
+POSTGRES_PASSWORD=${POSTGRES_USER:-postgres}
+
 # update user
 psql -v ON_ERROR_STOP=0 --dbname=${POSTGRES_DB:-postgres} --username=${POSTGRES_USER:-postgres} <<-EOQUERY
-    REVOKE CREATE ON SCHEMA public FROM public
-    ALTER USER ${POSTGRES_USER:-postgres} WITH ENCRYPTED PASSWORD '${POSTGRES_PASSWORD:-postgres}';
-    GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB:-postgres} TO ${POSTGRES_USER:-postgres};
+
+CREATE ROLE viewonly;
+CREATE ROLE readonly;
+CREATE ROLE readwrite;
+
+CREATE SCHEMA datamarts;
+
+-- REVOKE ACCESS
+REVOKE ALL ON ALL TABLES IN SCHEMA PUBLIC, datamarts FROM PUBLIC, viewonly, readonly, readwrite;
+REVOKE ALL ON ALL SEQUENCES IN SCHEMA PUBLIC, datamarts FROM PUBLIC, viewonly, readonly, readwrite;
+REVOKE ALL ON ALL FUNCTIONS IN SCHEMA PUBLIC, datamarts FROM PUBLIC, viewonly, readonly, readwrite;
+
+ALTER DEFAULT PRIVILEGES REVOKE ALL ON TABLES FROM PUBLIC, viewonly, readonly, readwrite;
+ALTER DEFAULT PRIVILEGES REVOKE ALL ON SEQUENCES FROM PUBLIC, viewonly, readonly, readwrite;
+ALTER DEFAULT PRIVILEGES REVOKE ALL ON FUNCTIONS FROM PUBLIC, viewonly, readonly, readwrite;
+
+REVOKE ALL ON SCHEMA PUBLIC, datamarts FROM PUBLIC, viewonly, readonly, readwrite;
+REVOKE ALL ON DATABASE ${POSTGRES_DB:-postgres} FROM PUBLIC, viewonly, readonly, readwrite;
+REVOKE ALL ON DATABASE postgres FROM PUBLIC, viewonly, readonly, readwrite;
+
+-- GRANT
+GRANT CONNECT ON DATABASE ${POSTGRES_DB:-postgres} TO viewonly, readonly, readwrite;
+GRANT USAGE ON SCHEMA public, datamarts TO readonly, readwrite;
+GRANT USAGE ON SCHEMA datamarts TO viewonly;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA datamarts TO viewonly ;
+GRANT SELECT ON ALL TABLES IN SCHEMA public, datamarts TO readonly ;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public, datamarts TO readwrite ;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA datamarts GRANT SELECT ON TABLES TO PUBLIC, viewonly;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public, datamarts GRANT SELECT ON TABLES TO readonly;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public, datamarts GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO readwrite;
+
+ALTER DEFAULT PRIVILEGES GRANT SELECT ON SEQUENCES TO readonly;
+ALTER DEFAULT PRIVILEGES GRANT ALL ON SEQUENCES TO readwrite;
+
+ALTER DEFAULT PRIVILEGES GRANT EXECUTE ON FUNCTIONS TO readonly;
+ALTER DEFAULT PRIVILEGES GRANT ALL ON FUNCTIONS TO readwrite;
+
+-- USERS
+CREATE USER ${POSTGRES_USER:-postgres}_viewonly WITH PASSWORD '${POSTGRES_PASSWORD:0:8}' WITH ROLE viewonly;
+CREATE USER ${POSTGRES_USER:-postgres}_readonly WITH PASSWORD '${POSTGRES_PASSWORD:0:16}' WITH ROLE readonly;
+CREATE USER ${POSTGRES_USER:-postgres}_readwrite WITH PASSWORD '${POSTGRES_PASSWORD:0:32}' WITH ROLE readwrite;
+
 EOQUERY
 
-# create readwrite role
-psql -v ON_ERROR_STOP=0 --dbname=${POSTGRES_DB:-postgres} --username=${POSTGRES_USER:-postgres} <<-EOQUERY
-    CREATE ROLE readwrite;
-    GRANT CONNECT ON DATABASE ${POSTGRES_DB:-postgres} TO readwrite;
-    GRANT USAGE ON SCHEMA public TO readwrite;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO readwrite;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE ON SEQUENCES TO readwrite;
-    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES IN SCHEMA public TO readwrite;
-    GRANT USAGE ON SEQUENCES IN SCHEMA public TO readwrite;
-EOQUERY
-
-# create readwrite user
-psql -v ON_ERROR_STOP=0 --dbname=${POSTGRES_DB:-postgres} --username=${POSTGRES_USER:-postgres} <<-EOQUERY
-    CREATE USER ${POSTGRES_USER:-postgres}_readwrite WITH ENCRYPTED PASSWORD '${POSTGRES_PASSWORD:-postgres}';
-    GRANT readwrite TO ${POSTGRES_USER:-postgres}_readwrite;
-EOQUERY
-
-# create readonly role
-psql -v ON_ERROR_STOP=0 --dbname=${POSTGRES_DB:-postgres} --username=${POSTGRES_USER:-postgres} <<-EOQUERY
-    CREATE ROLE readonly;
-    GRANT CONNECT ON DATABASE ${POSTGRES_DB:-postgres} TO readonly;
-    GRANT USAGE ON SCHEMA public TO readonly;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readonly;
-    GRANT SELECT ON TABLES IN SCHEMA public TO readonly;
-EOQUERY
-
-# create readonly user
-psql -v ON_ERROR_STOP=0 --dbname=${POSTGRES_DB:-postgres} --username=${POSTGRES_USER:-postgres} <<-EOQUERY
-    CREATE USER ${POSTGRES_USER:-postgres}_readonly WITH ENCRYPTED PASSWORD '${POSTGRES_PASSWORD:-postgres}';
-    GRANT readonly TO ${POSTGRES_USER:-postgres}_readonly;
-EOQUERY
-
-# create viewonly role
-psql -v ON_ERROR_STOP=0 --dbname=${POSTGRES_DB:-postgres} --username=${POSTGRES_USER:-postgres} <<-EOQUERY
-    CREATE ROLE viewonly;
-    CREATE SCHEMA views;
-    GRANT CONNECT ON DATABASE ${POSTGRES_DB:-postgres} TO viewonly;
-    GRANT USAGE ON SCHEMA views TO viewonly;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA views GRANT SELECT ON TABLES TO viewonly;
-    GRANT SELECT ON TABLES IN SCHEMA public TO readonly;
-EOQUERY
-
-# create viewonly user
-psql -v ON_ERROR_STOP=0 --dbname=${POSTGRES_DB:-postgres} --username=${POSTGRES_USER:-postgres} <<-EOQUERY
-    CREATE USER ${POSTGRES_USER:-postgres}_viewonly WITH ENCRYPTED PASSWORD '${POSTGRES_PASSWORD:-postgres}';
-    ALTER USER ${POSTGRES_USER:-postgres}_viewonly set search_path = 'views';
-    GRANT viewonly TO ${POSTGRES_USER:-postgres}_viewonly;
-EOQUERY
