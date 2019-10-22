1836e9a6917a6d9dc71b6e048ada62610577cfba
nullhack <eric.oliveira.lopes@gmail.com>
2019-10-22 21:48:58 +0800

    updating lab

--- lab/src/entrypoint.sh
+++ lab/src/entrypoint.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
-echo root:${ROOT_PASSWORD:?ROOT_PASSWORD} | chpasswd
+if [ ! -z "$ROOT_PASSWORD" ]; then
+  echo "Changing root password"
+  echo root:${ROOT_PASSWORD} | chpasswd
+fi
 
 printenv | grep LOGIN | sed -e 's/[^=]*=\(.*\)/\1/' | while read p; do
 if [ ! -z "$p" ]; then
   new_login=$p
   new_user=$(echo $new_login | sed -e 's/\(.*\):.*/\1/')
-  echo "Adding $new_user"
+  echo "Adding user: $new_user"
   useradd --shell /bin/bash --create-home $new_user
   echo $new_login | chpasswd
 fi
 done
+
+if [ ! -z "$CUSTOM_PACKAGES" ]; then
+  echo "Installing custom packages"
+  pip3 install $(echo $CUSTOM_PACKAGES | tr -s '(,|\n)' ' ' )
+fi
 
 /usr/sbin/sshd -D &
 
