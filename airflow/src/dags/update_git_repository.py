745efe133812a8d180e66e06ad7b4a4555909a2b
nullhack <eric.oliveira.lopes@gmail.com>
2020-05-24 04:39:17 +0800

    dag for updating git repository

---
+++ airflow/src/dags/update_git_repository.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.bash_operator import BashOperator
+
+bash_cmd = """
+DAGS_PATH='{{var.value.git_path}}'
+GIT_URL='{{var.value.git_url}}'
+
+mkdir -p "$DAGS_PATH"
+
+cd "$DAGS_PATH"
+
+if [ -d "$DAGS_PATH/.git" ]; then
+  rm -rf "$DAGS_PATH/.git"
+fi
+
+git init
+GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -i /run/secrets/ssh_github_dags' git remote add -f origin $GIT_URL
+git checkout master
+git merge origin/master --allow-unrelated-histories
+
+"""
+
+with DAG(
+    dag_id="update_git_repository",
+    start_date=days_ago(0),
+    schedule_interval='@once',
+    tags=['github', 'dags'],
+) as parent_dag:
+    task = BashOperator(task_id="parent_task",
+                        bash_command=bash_cmd,
+                        env={})
+
