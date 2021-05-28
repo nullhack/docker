5d3982f46bc5f7dc9d25732f7ffca5d40194f15e
Eric Lopes <nullhack@users.noreply.github.com>
2021-05-29 06:42:01 +0800

    minio v1.0

--- minio/build.sh
+++ minio/build.sh
@@ -1,9 +1,12 @@
+cd ../traefik
+sudo docker-compose up -d
+cd -
 cd ../ca-maker/
 sudo docker build -t localhost:5000/ca:registry .
-sudo docker run -ti --rm -v minio-certs:/certs/server -v minio-ca:/certs/ca -e GEN_NAME_KEY=private -e GEN_NAME_CRT=public localhost:5000/ca:registry
+sudo docker run -ti --rm -v minio-certs:/certs/server -v minio-ca:/certs/ca -e HOST=localhost -e GEN_NAME_KEY=private -e GEN_NAME_CRT=public localhost:5000/ca:registry
 cd -
 sudo docker-compose down -v
 sudo docker-compose up -d
 
-sudo docker run -ti --rm -v minio-ca:/certs ubuntu cat /certs/ca.crt > ca.crt
-export SSL_CERT_FILE=$PWD/ca.crt
+sudo docker run -ti --rm -v minio-ca:/certs ubuntu cat /certs/ca.crt > /tmp/ca.crt
+export SSL_CERT_FILE=/tmp/ca.crt
--- minio/create_certs.sh
+++
@@ -1,6 +0,0 @@
-sudo docker network create traefik-public
-
-sudo docker volume create minio-certs
-
-sudo docker run -ti --rm -v minio-certs:/certs -u root bitnami/minio:2021 bash -c "openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj '/C=US/ST=CA/L=SF/O=docker-ca/CN=minio.local' -keyout /certs/private.key -out /certs/public.crt"
-
