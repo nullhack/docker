e2bc4d638c1ac2c002f7dac0a59a9f74f5dffbc4
Eric Lopes <nullhack@users.noreply.github.com>
2022-03-05 03:25:44 +0800

    changes to registry

--- registry/build.sh
+++ registry/build.sh
@@ -3,7 +3,5 @@ bash build.sh
 cd -
 [ -n "$(sudo docker container ls -q -f name=registry)" ] || sudo docker-compose up -d
 
-sleep 1
-
-sudo docker login --username user --password password "registry.${HOST:-localhost}"
+# sudo docker login --username user --password password "registry.localhost"
 
